7bd5a68f9b530dba68a47d77807ce9f2d70a5a7f
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-01-20 07:16:42 -0300

    Criando menu e refatorando funcoes

--- ProgramacaoFuncional/Program.cs
+++ ProgramacaoFuncional/Program.cs
@@ -1,4 +1,14 @@
 ﻿
+static void Tabuada(int numero)
+{
+    Console.WriteLine($" ===== Tabuada do numero {numero}! =====");
+    for(int i = 1; i<=10; i++)
+    {
+        Console.WriteLine(numero + " X " + i + " = " + (numero*i));
+    }
+}
+
+
 int SAIDA_PROGRAMA = 0;
 int LER_ARQUIVOS = 1;
 int TABUADA = 2;
@@ -6,19 +16,39 @@ int CALCULO_MEDIA = 3;
 
 while (true)
 {
-    string mensagem = "Olá uusário, bem vindo ao programa, utilizando programação funcional" +
-        "\nDigite uma das opções abaixo:" +
-        "\n0 - Sair do programa" +
-        "\n1 - Para ler arquivos" +
-        "\n2 - Para executar tabuada" +
-        "\n3 - Calcular média de alunos";
+    string mensagem = "Olá uusário, bem vindo ao programa\n" + 
+        "\n ==== Programação funcional ====" +
+        "\n  Digite uma das opções abaixo:" +
+        "\n  0 - Sair do programa" +
+        "\n  1 - Para ler arquivos" +
+        "\n  2 - Para executar tabuada" +
+        "\n  3 - Calcular média de alunos";
     Console.WriteLine(mensagem);
 
-    Console.WriteLine($"Digite {SAIDA_PROGRAMA} para sair do programa");
-    int valor = int.Parse( Console.ReadLine() );
-
-    if (SAIDA_PROGRAMA == valor)
+    int valor = int.Parse(Console.ReadLine());
+             
+    if (valor == SAIDA_PROGRAMA)
     {
         break;
+    }
+    else if(valor == LER_ARQUIVOS)
+    {
+        Console.WriteLine(" ===== Ler Arquivos =====");
+        
+    }
+    else if(valor == TABUADA)
+    {
+        Console.WriteLine(" ===== Tabuada =====");
+        Console.WriteLine("Digite o numero que deseja na tabuada");
+        int numero = int.Parse(Console.ReadLine());
+        Tabuada(numero);
+    }
+    else if(valor == CALCULO_MEDIA)
+    {
+        Console.WriteLine("Falta implementar o calculo da média!");
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida, digite novamente!");
     }
 }
