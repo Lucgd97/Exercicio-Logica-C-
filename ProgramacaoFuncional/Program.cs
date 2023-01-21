1ee56ed6cd2436fd82293ac204b53f0ffac4b884
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-01-21 07:45:08 -0300

    Refatorando Projeto de Orientacao a funcao

--- ProgramacaoFuncional/Program.cs
+++ ProgramacaoFuncional/Program.cs
@@ -1,5 +1,29 @@
 ﻿
-static void Tabuada(int numero)
+static void LerArquivos(int numeroArquivo)
+{
+    string arquivoComCaminho = @"C:\Users\lucas\OneDrive\Área de Trabalho\Alura\C# Danilo\ProgramacaoFuncional\arq1.txt" + numeroArquivo + ".txt";
+    Console.WriteLine(" ====== Lendo Arquivo ======\n" + arquivoComCaminho + "\n====================");
+    if (File.Exists(arquivoComCaminho))
+    {
+        using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+        {
+            string linha;
+            while ((linha = arquivo.ReadLine()) != null)
+            {
+                Console.WriteLine(linha);
+            }
+        }
+    }
+
+    string arquivosComCaminho2 = @"C:\Users\lucas\OneDrive\Área de Trabalho\Alura\C# Danilo\ProgramacaoFuncional\arq2.txt" + numeroArquivo + ".txt";
+    if(File.Exists(arquivosComCaminho2))
+    {
+        LerArquivos(numeroArquivo + 1);
+    }
+    }
+
+
+    static void Tabuada(int numero)
 {
     Console.WriteLine($" ===== Tabuada do numero {numero}! =====");
     for(int i = 1; i<=10; i++)
@@ -34,14 +58,16 @@ while (true)
     else if(valor == LER_ARQUIVOS)
     {
         Console.WriteLine(" ===== Ler Arquivos =====");
-        
+        LerArquivos(1);
+        Console.WriteLine("\n========================\n");
     }
     else if(valor == TABUADA)
     {
-        Console.WriteLine(" ===== Tabuada =====");
+        Console.WriteLine(" ======== Tabuada ========");
         Console.WriteLine("Digite o numero que deseja na tabuada");
         int numero = int.Parse(Console.ReadLine());
         Tabuada(numero);
+        Console.WriteLine("\n========================\n");
     }
     else if(valor == CALCULO_MEDIA)
     {
